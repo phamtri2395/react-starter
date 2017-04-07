80739b580da48ca9bb8eb439ffcfe4e2c802ebfd
phamtri2395 <phamtri2395@gmail.com>
2017-04-07 21:11:21 +0700

    fix multi components path in styleguidist config

--- tools/config/styleguide.config.js
+++ tools/config/styleguide.config.js
@@ -3,9 +3,11 @@
  */
 
 const path = require('path');
+const glob = require('glob');
 const environment = require('./config').story.environment;
 const packageName = require('../../package.json').name;
 
+const src_path = path.resolve(__dirname, '../../src');
 const lookup_extension = 'jsx';
 const stories_extension = 'story.md';
 const stories_path = path.resolve(__dirname, '../../documents/stories');
@@ -15,10 +17,18 @@ module.exports = {
   serverHost: environment.host,
   serverPort: environment.port,
   webpackConfig: require('./webpack.config.dev'),
-  components: () => ([
-    `../../src/components/**/*.${lookup_extension}`,
-    `../../src/containers/**/components/*.${lookup_extension}`
-  ]), // Where to look for components
+  components: () => {
+    // Find all components folder in src/components/**/*.jsx & src/containers/**/components/**/*.jsx
+    const abs_path = glob.sync(`${src_path}/components/**/*.${lookup_extension}`).concat(
+      glob.sync(`${src_path}/containers/**/components/**/*.${lookup_extension}`)
+    );
+    // Turn absolute paths to relative path, which works with styleguide config
+    const rel_path = abs_path.map(dir => (
+      path.relative(__dirname, dir)
+    ));
+
+    return rel_path;
+  }, // Where to look for components
   getExampleFilename: function(componentPath) {
     const filename = path.basename(componentPath, `.${lookup_extension}`);
 
